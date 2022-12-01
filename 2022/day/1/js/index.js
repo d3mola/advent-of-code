6c82c3987067b92141ac90c49cceab3f795b7375
Ademola Ariya <ariyaademola@gmail.com>
2022-12-01 04:56:08 -0500

    day 1 - part 2

--- 2022/day/1/js/index.js
+++ 2022/day/1/js/index.js
@@ -1,28 +1,29 @@
 const fs = require('fs')
 
-const INPUT_FILE_PATH = '../smol-input.txt'
-// const INPUT_FILE_PATH = '../input.txt'
+// const INPUT_FILE_PATH = '../smol-input.txt'
+const INPUT_FILE_PATH = '../input.txt'
 
-// function top3ElvesCalories(elvesCalories) {
-//     let max = maxCalories(elvesCalories)
-// }
+function top3ElvesCalories(elvesCalories) {
+    return maxCalories(elvesCalories, 3)
+}
 
 let elvesCalories = inputToArray(readInput())
 
-function maxCalories(elvesCalories) {
-    let max = 0
+function maxCalories(elvesCalories, numElves = 1) {
+    let total = 0
+    let descendingCalories = []
 
     for (let elfCalories of elvesCalories) {
         let totalElfCalories = calcTotalElfCalories(elfCalories)
-        if (totalElfCalories > max) {
-            max = totalElfCalories
-        }
+        descendingCalories.push(totalElfCalories)
     }
 
-    return max
+    descendingCalories.sort((a, b) => b - a)
+    return calcTotalElfCalories(descendingCalories.slice(0, numElves))
 }
 
 console.log(`Elf carrying highest calories has ${maxCalories(elvesCalories)} calories`)
+console.log(`The top 3 elves are carrying ${top3ElvesCalories(elvesCalories)} calories`)
 
 function calcTotalElfCalories(calories) {
     return calories.reduce((prev, current) => prev + current, 0)
