940797769b7a01117d0cdeb6c03f73b10ed88981
Ademola Ariya <ariyaademola@gmail.com>
2022-12-06 00:32:29 -0500

    day 6 part 1

---
+++ 2022/day-06/index.ts
@@ -0,0 +1,23 @@
+import assert from 'assert'
+import fs from 'fs'
+import path from 'path'
+
+console.log('Day 06')
+part1()
+part2()
+
+function part1() {
+    let input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('')
+    let result = 0
+    for (let i=0; i<input.length; i++) {
+        let sequenceof4 = input.slice(i, i+4)
+        if (new Set(sequenceof4).size === sequenceof4.length) { // no duplicate in sequence
+            result = i + 4
+            break
+        }
+    }
+    assert.equal(result, 1134)
+}
+
+function part2() {
+}
